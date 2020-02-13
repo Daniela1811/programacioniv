bf3e8fa3c1b57cade8bacca2ae690eb8ed4476b3
Daniela1811 <drm10651@gmail.com>
2020-02-13 00:48:28 -0600

    Alumnos

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", e=>{
     document.querySelector("#frmAlumnos").addEventListener("submit", event=>{
         event.preventDefault();
+        
+        var codigo =  JSON.parse(localStorage.getItem("codigo"));
+        var codigo = prompt("New member codigo?");
+        names[++index] = Codigo;
+        localStorage.setItem("codigo ", JSON.stringify(codigo));
+
+        var nombre =  JSON.parse(localStorage.getItem("nombre"));
+        var nmbre = prompt("New member nombre?");
+        names[++index] = nombre;
+        localStorage.setItem("nombre", JSON.stringify(nombre));
+
+        var direccion =  JSON.parse(localStorage.getItem("direccion"));
+        var direccion = prompt("New member direccion?");
+        names[++index] = direccion;
+        localStorage.setItem("direccion ", JSON.stringify(direccion));
+
+        var telefono=  JSON.parse(localStorage.getItem("telefono"));
+        var telefono = prompt("New member telefono?");
+        names[++index] = telefono;
+        localStorage.setItem("telefono ", JSON.stringify(telefono));
+
+
+
+
 
         let codigo = document.querySelector("#txtCodigoAlumno").value,
             nombre = document.querySelector("#txtNombreAlumno").value,
@@ -8,7 +32,7 @@ document.addEventListener("DOMContentLoaded", e=>{
             telefono = document.querySelector("#txtTelefonoAlumno").value;
 
         console.log(codigo, nombre, direccion, telefono);
-        
+
         if( 'localStorage' in window ){
             window.localStorage.setItem("codigo", codigo);
             window.localStorage.setItem("nombre", nombre);
