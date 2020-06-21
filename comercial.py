f0185a2fc1a65b9b2ac5d543d37be27f52410195
Daniela1811 <drm10651@gmail.com>
2020-06-21 00:40:54 -0600

    comercial

---
+++ comercial.py
@@ -0,0 +1,14 @@
+#UNIVO
+from facebook_scraper import get_posts
+import csv
+
+face = csv.writer(open('comercial.csv','w'))
+face.writerow(['POST_ID','TEXTO','LIKES', ])
+
+for post in get_posts('ComercialFuentes01 ', pages=5, ):
+    #print(post.keys())
+    print(post['post_id'], post['text'], post['likes'], sep=' - ')
+    try:
+        face.writerow([post['post_id'], post['text'], post['likes']])
+    except:
+        None
