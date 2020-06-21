db431552294ce6be245e6a23dc6e2f2958586943
Daniela1811 <drm10651@gmail.com>
2020-06-20 23:32:49 -0600

    cdmype

--- cdmype.py
+++ cdmype.py
@@ -3,13 +3,13 @@ from facebook_scraper import get_posts
 import csv
 
 face = csv.writer(open('cdmype.csv','w'))
-face.writerow(['POST_ID','TEXTO','LIKES','comments'])
+face.writerow(['POST_ID','TEXTO','LIKES'])
 
 for post in get_posts('cdmypeugbusulutan ', pages=10, ):
     #print(post.keys())
-    print(post['post_id'], post['text'],post['Likes'], post['comments'], sep=' - ')
+    print(post['post_id'], post['text'],post['Likes'],  sep=' - ')
     try:
-        face.writerow([post['post_id'], post['text'], post['Likes'], post['comments']])
+        face.writerow([post['post_id'], post['text'], post['Likes']])
     except:
         None
         
