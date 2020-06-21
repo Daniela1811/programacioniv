7d4fef4efb73f186505025403641422a8138172d
Daniela1811 <drm10651@gmail.com>
2020-06-20 22:06:55 -0600

    pruebas

---
+++ cdmype.py
@@ -0,0 +1,15 @@
+#UNIVO
+from facebook_scraper import get_posts
+import csv
+
+face = csv.writer(open('cdmype.csv','w'))
+face.writerow(['POST_ID','TEXTO','LIKES'])
+
+for post in get_posts('cdmypeugbusulutan ', pages=10, ):
+    #print(post.keys())
+    print(post['post_id'], post['text'], post['likes'], sep=' - ')
+    try:
+        face.writerow([post['post_id'], post['text'], post['comments'], post['likes']])
+    except:
+        None
+        
