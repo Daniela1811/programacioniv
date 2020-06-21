8fcd3104f3a32fb46c39024cacdd6ff6939955cf
Daniela1811 <drm10651@gmail.com>
2020-06-21 00:42:41 -0600

    comercial

--- comercial.py
+++ comercial.py
@@ -5,7 +5,7 @@ import csv
 face = csv.writer(open('comercial.csv','w'))
 face.writerow(['POST_ID','TEXTO','LIKES', ])
 
-for post in get_posts('ComercialFuentes01 ', pages=5, ):
+for post in get_posts('ComercialFuentes01 ', pages=10, ):
     #print(post.keys())
     print(post['post_id'], post['text'], post['likes'], sep=' - ')
     try:
