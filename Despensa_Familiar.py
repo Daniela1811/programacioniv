bdb1538133072839f94ec4a232f3036573363bbc
Daniela1811 <drm10651@gmail.com>
2020-06-21 16:15:53 -0600

    Bd

--- Despensa_Familiar.py
+++ Despensa_Familiar.py
@@ -1,12 +1,12 @@
-#Cdmype
+#DESSPENSA_Familiar
 
 from facebook_scraper import get_posts
 import csv
 
-face = csv.writer(open('cdmype.csv','w'))
+face = csv.writer(open('Despensa_Familiar.csv','w'))
 face.writerow(['POST_ID','TEXTO','LIKES'])
 
-for post in get_posts('cdmypeugbusulutan', pages=5, ):
+for post in get_posts('DespensaFamiliarElSalvador', pages=10, ):
     #print(post.keys())
     print(post['post_id'], post['text'], post['likes'], sep=' - ')
     try:
--- Walmart.py
+++ Walmart.py
@@ -1,11 +1,12 @@
-#UNAB
+#DESSPENSA
+
 from facebook_scraper import get_posts
 import csv
 
-face = csv.writer(open('UNAB.csv','w'))
-face.writerow(['POST_ID','TEXTO','LIKES', ])
+face = csv.writer(open('walmart .csv','w'))
+face.writerow(['POST_ID','TEXTO','LIKES'])
 
-for post in get_posts('UNABElSalvador ', pages=5, ):
+for post in get_posts('walmart ', pages=10, ):
     #print(post.keys())
     print(post['post_id'], post['text'], post['likes'], sep=' - ')
     try:
